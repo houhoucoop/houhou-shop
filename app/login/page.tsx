968e8be3ba32f18a22de01cfba4e69c0bc4323bd
Yiya <yiya.chen@ibm.com>
2024-05-24 21:19:59 +0800

    feat: frontend UI

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Authentication',
+  description: 'Authentication forms built using the components.',
+};
+
+export default function Page() {
+  return <>dsads</>;
+}
---
+++ components/page-header.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import NextLink from 'next/link';
+import Image from 'next/image';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { UserNav } from './user-nav';
+import logoImage from '../src/images/logo.png';
+
+export default function PageHeader() {
+  return (
+    <header className="w-full border-b border-border/40">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <div className="mr-4 hidden md:flex">
+          <div className="mr-4 w-10">
+            <Image
+              src={logoImage}
+              style={{ objectFit: 'cover' }}
+              alt="Logo"
+              className="block dark:hidden"
+            />
+          </div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NextLink href="/" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    README
+                  </NavigationMenuLink>
+                </NextLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NextLink href="/tasks" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Tasks
+                  </NavigationMenuLink>
+                </NextLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <UserNav />
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
+    </header>
+  );
+}
